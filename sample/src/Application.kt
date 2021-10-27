50f0378068b3ad085cd608ce14780228b51ed646
Jacopo Franzoi <jacopo.franzoi@gucci.com>
2021-10-27 21:38:40 +0200

    moved to multi-module project; lib and sample

---
+++ sample/src/Application.kt
@@ -0,0 +1,28 @@
+package com.gucci
+
+import com.gucci.polling.Ids
+import com.gucci.polling.LongRunning
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.jackson.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import kotlinx.coroutines.delay
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@kotlin.jvm.JvmOverloads
+fun Application.module() {
+    install(DoubleReceive) {
+        receiveEntireContent = true
+    }
+
+    routing {
+        route("/api/executions", LongRunning(Ids.Sequential()).with({
+            delay(10000)
+            val name: Any = it.call().request.queryParameters["name"] ?: "Bob"
+            return@with "Hello, $name"
+        }))
+    }
+}
+
